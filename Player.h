25b06a1eb411f9632382ca64f2a78f7546390d8e
AntoineBardy <antoine.bardy11@gmail.com>
2021-03-10 10:58:28 +0100

    Update Player.h

--- Player.h
+++ Player.h
@@ -8,7 +8,7 @@ class Player
         int _score[5] = {0,30,90,40,60};
 
     public:
-        void addScore(int score, int chanson);
+        void Score(int score, int chanson);
         void AfficherScore();
         void printMeilleurScore(); 
         void printScoreTotal(); 
